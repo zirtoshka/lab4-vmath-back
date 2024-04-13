688143b9f005c87f9f59221df7b299dcceac5dd0
zirtoshka <367222@edu.itmo.ru>
2024-04-13 13:30:00 +0300

    Initial commit

---
+++ src/main/java/zir/lab4vmathback/Lab4VmathBackApplication.java
@@ -0,0 +1,13 @@
+package zir.lab4vmathback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab4VmathBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab4VmathBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/zir/lab4vmathback/Lab4VmathBackApplicationTests.java
@@ -0,0 +1,13 @@
+package zir.lab4vmathback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab4VmathBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
